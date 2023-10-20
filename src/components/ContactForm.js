6683cc72f2dbed6afb7fd8392bd4862ef10c13f3
Emily <emelizadaniels@gmail.com>
2023-10-20 12:30:47 -0400

    added emailjs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,31 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 function ContactForm() {
+const form = useRef();
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_1995jun3s3v3n', 'template_1995jun3s3v3n', form.current, 'eeZ_8VbzfeTHUtfhv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+};
+
+
+
     return (
         <div>
-            <form>
-                <input type="text" placeholder="Your Name"></input>
-                <input type="email" placeholder="Your email"></input>
-                <input type="text" placeholder="Your Message"></input>
+            <form ref= { form } onSubmit={ sendEmail } className="contact-form">
+                <input type="text" placeholder="Your Name" name="user_name"></input>
+                <input type="email" placeholder="Your email" name="user_email"></input>
+                <textarea type="text" placeholder="Your Message" name="message"></textarea>
+                <input type="submit" value="Send"></input>
             </form>
+        
 
         </div>
     )
