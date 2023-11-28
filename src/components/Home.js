5cbd2151b2016f442e5f4ea79c50f8958ff76563
Emily <emelizadaniels@gmail.com>
2023-11-28 11:16:09 -0500

    added nav img and self img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,24 @@
-import GenArt from './GenArt';
-import Contact from './Contact';
 
-
+import emily from '../images/emily.jpg'
+import sunsAndClouds from '../images/suns_and_clouds.png'
 
 function Home() {
     return (
         <div>
             <div className="name-div">
-            <h2>Emily Daniels</h2>
+            <h2>Emily</h2>
+            <h2>Daniels</h2>
             </div>
-            <h3>STEAM Educator and Fullstack Software Engineer</h3>
-            <GenArt />
-            <Contact />
+            <div className="image-div">
+                <img className="emily-image"src={emily} alt="photo of Emily"/>
+            </div>
+            <div clasName="text-div">
+                <h2>Hi, there! I'm Emily</h2>
+                <h3>I am a full-stack engineer / web dev / educator,</h3>
+                <br></br>
+                <h3>currently residing in Brooklyn, NY.</h3>
+            </div>
+            
             
         </div>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
+import sunsAndClouds from '../images/suns_and_clouds.png';
 
 function Nav() {
   return (
-    <div className="nav-bar">
+    
       <nav>
         <Link to="/about">About</Link>
         <Link to="/resume">Resume</Link>
+        <div className="nav-img">
+        <img className="suns-and-clouds"src={sunsAndClouds}/>
+        <img className="suns-and-clouds"src={sunsAndClouds}/>
+        <img className="suns-and-clouds"src={sunsAndClouds}/>
+        </div>
         <Link to="/blog">Blog</Link>
         <Link to="/projects">Projects</Link>
       </nav>
-    </div>
+
   );
 }
 
