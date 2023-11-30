1ea6965a05d4d9ec2d5ac03963037d5a1c6c0a2d
Emily <emelizadaniels@gmail.com>
2023-11-30 13:36:34 -0500

    styling to landign page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,27 @@ import clouds from '../images/cloudies.png';
 
 function Home() {
     return (
-        <div>
+        <div className="home-div">
             <div className="name-div">
             <h2>Emily</h2>
             <h2>Daniels</h2>
             </div>
             <div className="image-div">
                 <img className="emily-image"src={emily} alt="photo of Emily"/>
-                <img src={clouds}/>
-                <img src={clouds}/>
-                <img src={sun}/>
-                <img src={sun}/>
-                <img src={sun}/>
+                <img className="cloud-1" src={clouds}/>
+                <img  className="cloud-2" src={clouds}/>
+                <img  className="sun-1"src={sun}/>
+                <img className="sun-2" src={sun}/>
+                <img className="sun-3"src={sun}/>
+            
             </div>
-            <div clasName="text-div">
+
+            <div className="text-div">
                 <h2>Hi, there! I'm Emily</h2>
                 <h3>I am a full-stack engineer / web dev / educator,</h3>
                 <br></br>
                 <h3>currently residing in Brooklyn, NY.</h3>
-            </div>
-            
+            </div> 
             
         </div>
     )
