c7f366600ecd08888b9b33845a0247a80ccbe86f
Emily <emelizadaniels@gmail.com>
2024-03-20 12:45:10 -0400

    lil sum

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './style.css';
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
-
-root.render(
+ReactDOM.render(
     <Router>
         <App />
-    </Router>    
+    </Router>,
+    document.getElementById('root')
 );
-
